465d74ea28e08ae1fe532b04b6b820f00ae7e1d7
Landlucas <land.lucas@outlook.com>
2023-10-20 16:19:09 -0300

    serp_client now exports results

--- serp_client.py
+++ serp_client.py
@@ -1,44 +1,49 @@
 import requests
 import pandas as pd
 
-# Define a URL do serviço web
 web_service_url = "http://172.18.92.237:5000"
 
-# Carrega o dataset em formato CSV
-df = pd.read_csv('serp_ai_gen_processed_dataset_sample.csv')
+df = pd.read_csv("serp_client_test_sample.csv")
 
-# Espera o pressionamento de uma tecla para iniciar as iteracoes
 input("Press any key to start...")
 
-# Itera sobre cada linha no DataFrame
-for index, row in df.iterrows():
-    query = row['query']
-    rank = row['rank']
-    url = row['url']
-    
-    # Cria o corpo da requisição
-    request_body = {
-        'query': query,
-        'url': url
-    }
+result_data = []
 
-    # Envia uma requisição POST para o serviço web
+for index, row in df.iterrows():
+    query = row["query"]
+    rank = row["rank"]
+    url = row["url"]
+
+    request_body = {"query": query, "url": url}
+
     response = requests.post(web_service_url, json=request_body)
 
-    # Verifica se a requisição foi bem sucedida (código de status 200)
     if response.status_code == 200:
-        # Extrai os dados da resposta
         response_data = response.json()
-        answer_confidence_score = response_data['answer_confidence_score']
-        page_authority_score = response_data['page_authority_score']
-        trust_score = response_data['trust_score']
-        content = response_data['content']
+        answer_confidence_score = response_data["answer_confidence_score"]
+        page_authority_score = response_data["page_authority_score"]
+        trust_score = response_data["trust_score"]
+        content = response_data["content"]
 
-        # Imprime as informações
-        print(f"Query: {query}, Rank: {rank}, URL: {url}")
-        print(f"Answer Confidence Score: {answer_confidence_score}, Trust Score: {trust_score}, Page Authority Score: {page_authority_score}")
-        # print("-" * 30)
-        # print(content)
+        result_data.append({
+            "query": query,
+            "rank": rank,
+            "url": url,
+            "answer_confidence_score": answer_confidence_score,
+            "trust_score": trust_score,
+            "page_authority_score": page_authority_score
+        })
+
         print("-" * 30)
+        print(f"Query: {query}, Rank: {rank}, URL: {url}")
+        print(
+            f"Answer Confidence Score: {answer_confidence_score}, Trust Score: {trust_score}, Page Authority Score: {page_authority_score}"
+        )
     else:
         print(f"Error sending request for query '{query}' and URL '{url}'")
+
+print("-" * 30)
+
+result_df = pd.DataFrame(result_data)
+
+result_df.to_csv("serp_client_results.csv", index=False)
